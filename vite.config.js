79375033a72e139986c8cbb40a6a8e9fa4300fab
Bernardo Alonso <bernardo4430@gmail.com>
2024-05-08 12:59:00 -0500

    made initial settup for voice recognition

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './src/main.jsx' // Adjust the path to your actual entry point file
-      }
-    }
-  },
   server: {
     port: 3000,
     open: true
   }
-});
+})
